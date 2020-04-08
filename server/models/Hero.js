feadfe76a4a5b8adaa2131421a31b90247c4ee92
iihll <iihll.dev@gmail.com>
2020-04-08 21:21:24 +0800

    完成英雄模型

---
+++ server/models/Hero.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  name: { type: String },
+  avatar: { type: String },
+  title: { type: String },
+  // 字段变复数 类型变数组 表示关联多个分类
+  categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+  scores: {
+    // 难度
+    difficult: { type: Number },
+    // 技能
+    skills: { type: Number },
+    // 攻击
+    attack: { type: Number },
+    // 生存
+    survive: { type: Number }
+  },
+  // 技能
+  skills: [
+    {
+      icon: { type: String },
+      name: { type: String },
+      description: { type: String },
+      tips: { type: String }
+    }
+  ],
+  // 顺风装备字段
+  items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  // 逆风装备字段
+  items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  // 使用技巧
+  usageTips: { type: String },
+  // 对抗技巧
+  battleTips: { type: String },
+  // 团战思路
+  teamTips: { type: String },
+  // 英雄关系
+  // 最佳搭档
+  partners: [
+    {
+      hero: { type: mongoose.SchemaTypes.ObjectId, ref: 'Hero' },
+      description: { type: String }
+    }
+  ]
+})
+
+module.exports = mongoose.model('Hero', schema)
