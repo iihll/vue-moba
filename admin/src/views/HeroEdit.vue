2731f8f894a7d240011b3aa916b191febd496174
iihll <iihll.dev@gmail.com>
2020-04-08 21:20:59 +0800

    完成基本的英雄列表和编辑

---
+++ admin/src/views/HeroEdit.vue
@@ -0,0 +1,161 @@
+<template>
+  <div>
+    <h1>{{ id ? '编辑' : '新建' }}英雄</h1>
+    <el-form @submit.native.prevent="save" label-width="120px">
+      <el-form-item label="头像">
+        <el-upload
+          class="avatar-uploader"
+          :action="$http.defaults.baseURL + '/upload'"
+          :show-file-list="false"
+          :on-success="afterUpload"
+        >
+          <img v-if="model.avatar" :src="model.avatar" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="称号">
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item label="类型">
+        <el-select v-model="model.categories" multiple>
+          <el-option
+            v-for="item in categories"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="难度" class="difficult">
+        <el-rate :max="9" show-score v-model="model.scores.difficult"></el-rate>
+      </el-form-item>
+      <el-form-item label="技能" class="difficult">
+        <el-rate :max="9" show-score v-model="model.scores.skills"></el-rate>
+      </el-form-item>
+      <el-form-item label="攻击" class="difficult">
+        <el-rate :max="9" show-score v-model="model.scores.attack"></el-rate>
+      </el-form-item>
+      <el-form-item label="生存" class="difficult">
+        <el-rate :max="9" show-score v-model="model.scores.survive"></el-rate>
+      </el-form-item>
+      <el-form-item label="顺风出装">
+        <el-select v-model="model.items1" multiple>
+          <el-option v-for="item in items" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="逆风出装">
+        <el-select v-model="model.items2" multiple>
+          <el-option v-for="item in items" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="使用技巧">
+        <el-input type="textarea" v-model="model.usageTips"></el-input>
+      </el-form-item>
+      <el-form-item label="对抗技巧">
+        <el-input type="textarea" v-model="model.battleTips"></el-input>
+      </el-form-item>
+      <el-form-item label="团战思路">
+        <el-input type="textarea" v-model="model.teamTips"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {}
+  },
+  data() {
+    return {
+      model: {
+        name: "",
+        avatar: "",
+        title: "",
+        categories: [],
+        scores: {
+          difficult: 0
+        }
+      },
+      // 英雄类型
+      categories: [],
+      // 出装
+      items: []
+    };
+  },
+  created() {
+    this.id && this.fetch();
+    this.fetchCategories();
+    this.fetchItem();
+  },
+  methods: {
+    async fetchItem() {
+      const res = await this.$http.get(`rest/items`);
+      this.items = res.data;
+    },
+    async save() {
+      if (this.id) {
+        await this.$http.put(`rest/heroes/${this.id}`, this.model);
+      } else {
+        await this.$http.post("rest/heroes", this.model);
+      }
+      this.$router.push("/heroes/list");
+      this.$message.success("保存成功");
+    },
+    async fetch() {
+      const res = await this.$http.get(`rest/heroes/${this.id}`);
+      this.model = Object.assign({}, this.model, res.data);
+    },
+    async fetchCategories() {
+      const res = await this.$http.get(`rest/categories`);
+      this.categories = res.data;
+    },
+    afterUpload(res) {
+      // console.log(res);
+      // vue 显式赋值 model 未定义数据使用 $set
+      // this.$set(this.model, "avatar", res.url);
+      this.model.avatar = res.url;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 78px;
+  height: 78px;
+  line-height: 78px;
+  text-align: center;
+}
+.avatar {
+  width: 78px;
+  height: 78px;
+  display: block;
+}
+.difficult {
+  position: relative;
+  .el-rate {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>>
---
+++ admin/src/views/HeroList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>英雄列表</h1>
+    <el-table :data="items" stripe>
+      <el-table-column prop="_id" label="ID"></el-table-column>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="title" label="称号"></el-table-column>
+      <el-table-column label="头像">
+        <template v-slot="scope">
+          <el-image style="width: 3rem;" :src="scope.row.avatar"></el-image>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template v-slot="scope">
+          <el-button
+            type="warning"
+            size="mini"
+            @click="$router.push(`/heroes/edit/${scope.row._id}`)"
+          >编辑</el-button>
+          <el-button type="danger" size="mini" @click="remove(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created() {
+    this.fetch();
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get("rest/heroes");
+      this.items = res.data;
+    },
+    remove(row) {
+      this.$confirm(`此操作将永久删除${row.name}, 是否继续?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          await this.$http.delete(`rest/heroes/${row._id}`);
+          this.$message.success("删除成功");
+          this.fetch();
+        })
+        .catch(() => {
+          this.$message.info("已取消删除");
+        });
+    }
+  }
+};
+</script>
