a3c9ea6d95078da4a7b3036be4773eb3bdc0ef05
iihll <iihll.dev@gmail.com>
2020-04-10 18:06:45 +0800

    完成移动端公共头部

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入全局样式
+import './assets/scss/global.scss'
+
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
