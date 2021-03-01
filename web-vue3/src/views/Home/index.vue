d9f08e25639c2612dfef7ef66cfcb9f12e406131
duanqing.xing <duanqing.xing@cloudsdnet.com>
2021-03-01 17:03:52 +0800

    添加配置

--- web-vue3/src/views/Home/index.vue
+++ web-vue3/src/views/Home/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <div class="swiper-container">
+      <!-- <swiper :options="swiperOptions">
+        <swiper-slide>
+          <img src="../assets/images/slide1.jpeg" class="w-100" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/images/slide1.jpeg" class="w-100" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/images/slide1.jpeg" class="w-100" />
+        </swiper-slide>
+        <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+      </swiper> -->
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".pagination-home"
+          // type: 'custom',
+        },
+        autoplay: true
+      }
+    }
+  }
 }
 </script>
 
